f48893d4cd6b284a3ee76631ad349e46f4fa6eaa
Anthony Hoyas <anthony.hoyas@gmail.com>
2022-02-08 16:57:33 +0100

    working

--- script.js
+++ script.js
@@ -120,26 +120,97 @@ window.addEventListener('keydown', (event) => {
 
 // going down
 
+
 window.addEventListener('keydown', (event) => {
     if( event.key === 'ArrowDown') {
+        // let hero = document.querySelector('.hero')
+        
         for (const elementParent of LEVEL_1) {
-            //console.log(elementParent);
             for (const element of elementParent) {
                 //console.log(element);
                 let xofS = elementParent.indexOf('S')
-                if (element === "S") {
-                    //console.log('S');
-                    elementParent.splice(xofS-1, 1, "S")
+                if (element === "S" && LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1] != '*' ) {
+                    console.log(xofS)
+                    LEVEL_1.indexOf(elementParent)
                     elementParent.splice(xofS, 1, '.')
+                    LEVEL_1[LEVEL_1.indexOf(elementParent) + 1].splice(xofS, 1, 'S')
+                    console.log(LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1])
                     createMaze()
-                    //console.log(elementParent[xofS+2]);
                     break
                 }
-                else if (elementParent[xofS-1] === "*") {
-                    break;
-                } 
+                // else if ( LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1] === "*") {
+                //     break;
+                // } 
             }
         }
 
     }
 })
+
+
+// going up
+
+
+window.addEventListener('keydown', (event) => {
+    if( event.key === 'ArrowUp') {
+        // let hero = document.querySelector('.hero')
+        
+        for (const elementParent of LEVEL_1) {
+            for (const element of elementParent) {
+                //console.log(element);
+                let xofS = elementParent.indexOf('S')
+                if (element === "S") {
+                    // console.log(xofS)
+                    LEVEL_1.indexOf(elementParent)
+                    elementParent.splice(xofS, 1, '.')
+                    console.log(LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][xofS]);
+                    if (LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][xofS] === "*") {
+                        break
+                    } else {
+                    LEVEL_1[LEVEL_1.indexOf(elementParent) - 1].splice(xofS, 1, 'S')
+                    //console.log(LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1])
+                    console.log(xofS);
+                    createMaze()
+                    }
+                }
+
+            }
+        }
+
+    }
+})
+
+// window.addEventListener('keydown', (event) => {
+//     if( event.key === 'ArrowUp') {
+//         array = []
+//         for (const elementParent of LEVEL_1) {
+//            // console.log(elementParent);
+//             x = 0
+//           //  console.log(elementParent[x+1]);
+//             array.push(elementParent[x+1])
+//             console.log(array.length);
+//             for (const element of array) {
+                
+//                 let xofS = array.indexOf('S')
+//                 console.log(xofS);
+                
+//                 if (element === "S" && array.length === 12) {
+//                      console.log('S');
+//                     array.splice(xofS, 1, '.')
+//                     array.splice(xofS+1, 1, "S")
+//                 // console.log(elementParent[0]);
+//             //         elementParent.splice(xofS, 1, '.')
+//                       createMaze()
+//                       xofS += 1
+//             //     //     //console.log(elementParent[xofS+2]);
+//             //     //     break
+//             //     // }
+//             //     // else if (elementParent[xofS-1] === "*") {
+//             //     //     break;
+//               } 
+//             }
+//         }
+//         x += 1
+
+//     }
+// })
