2fce42ee5c808150e9cbbcc7d1f256266a8d4fbf
Anthony Hoyas <anthony.hoyas@gmail.com>
2022-02-09 11:12:34 +0100

    working maze

--- script.js
+++ script.js
@@ -39,8 +39,24 @@ const LEVEL_1 = [
   ]
 
 
+
+  clear = () =>  {  
+    let x = document.getElementById('maze')
+    x.remove()
+    // let main = document.querySelector('main')
+    // for (let i = 4; i >=1 ; i--) {
+    //     console.log('hi');
+    //     x.remove()
+        
+    // }
+}
+
+
     createMaze = () => {
         let main = document.querySelector('main')
+        let maze = document.createElement('div')
+        maze.setAttribute('id', 'maze')
+        main.append(maze)
         n = 0
         LEVEL_1.forEach(element => {
             let divx = document.createElement('div')
@@ -60,16 +76,18 @@ const LEVEL_1 = [
                         divy.setAttribute('class', 'treasure')
                     }
                 });
-            main.append(divx)
+            maze.append(divx)
             n += 1
         });
     }
+    
     createMaze()
 
 // going to Right
 
 window.addEventListener('keydown', (event) => {
     if( event.key === 'ArrowRight') {
+
         for (const elementParent of LEVEL_1) {
             //console.log(elementParent);
             for (const element of elementParent) {
@@ -79,6 +97,7 @@ window.addEventListener('keydown', (event) => {
                     //console.log('S');
                     elementParent.splice(xofS+1, 1, "S")
                     elementParent.splice(xofS, 1, '.')
+                    clear()
                     createMaze()
                     //console.log(elementParent[xofS+2]);
                     break
@@ -96,6 +115,7 @@ window.addEventListener('keydown', (event) => {
 
 window.addEventListener('keydown', (event) => {
     if( event.key === 'ArrowLeft') {
+
         for (const elementParent of LEVEL_1) {
             //console.log(elementParent);
             for (const element of elementParent) {
@@ -105,6 +125,7 @@ window.addEventListener('keydown', (event) => {
                     //console.log('S');
                     elementParent.splice(xofS-1, 1, "S")
                     elementParent.splice(xofS, 1, '.')
+                    clear()
                     createMaze()
                     //console.log(elementParent[xofS+2]);
                     break
@@ -123,24 +144,25 @@ window.addEventListener('keydown', (event) => {
 
 window.addEventListener('keydown', (event) => {
     if( event.key === 'ArrowDown') {
-        // let hero = document.querySelector('.hero')
+
         
         for (const elementParent of LEVEL_1) {
             for (const element of elementParent) {
-                //console.log(element);
                 let xofS = elementParent.indexOf('S')
-                if (element === "S" && LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1] != '*' ) {
-                    console.log(xofS)
-                    LEVEL_1.indexOf(elementParent)
-                    elementParent.splice(xofS, 1, '.')
-                    LEVEL_1[LEVEL_1.indexOf(elementParent) + 1].splice(xofS, 1, 'S')
-                    console.log(LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1])
+                if (element === "S" ) {
+                    console.log(LEVEL_1[LEVEL_1.indexOf(elementParent) + 2][xofS]);
+                    if ((LEVEL_1[LEVEL_1.indexOf(elementParent) + 1][xofS]) === '*') {
+                        //createMaze()
+                        break
+                    } else {
+                        LEVEL_1[LEVEL_1.indexOf(elementParent) + 1].splice(xofS, 1, 'S')
+                        elementParent.splice(xofS, 1, '.')
+                        clear()
                     createMaze()
-                    break
+                    return
+                    }
                 }
-                // else if ( LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1] === "*") {
-                //     break;
-                // } 
+
             }
         }
 
@@ -153,24 +175,25 @@ window.addEventListener('keydown', (event) => {
 
 window.addEventListener('keydown', (event) => {
     if( event.key === 'ArrowUp') {
-        // let hero = document.querySelector('.hero')
-        
+        clear()
+
         for (const elementParent of LEVEL_1) {
             for (const element of elementParent) {
-                //console.log(element);
                 let xofS = elementParent.indexOf('S')
                 if (element === "S") {
-                    // console.log(xofS)
                     LEVEL_1.indexOf(elementParent)
-                    elementParent.splice(xofS, 1, '.')
                     console.log(LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][xofS]);
                     if (LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][xofS] === "*") {
-                        break
+                        createMaze()
+
+                        return
                     } else {
+                        elementParent.splice(xofS, 1, '.')
                     LEVEL_1[LEVEL_1.indexOf(elementParent) - 1].splice(xofS, 1, 'S')
-                    //console.log(LEVEL_1[LEVEL_1.indexOf(elementParent) - 1][1])
                     console.log(xofS);
+                    //clear()
                     createMaze()
+                    return
                     }
                 }
 
@@ -180,37 +203,7 @@ window.addEventListener('keydown', (event) => {
     }
 })
 
-// window.addEventListener('keydown', (event) => {
-//     if( event.key === 'ArrowUp') {
-//         array = []
-//         for (const elementParent of LEVEL_1) {
-//            // console.log(elementParent);
-//             x = 0
-//           //  console.log(elementParent[x+1]);
-//             array.push(elementParent[x+1])
-//             console.log(array.length);
-//             for (const element of array) {
-                
-//                 let xofS = array.indexOf('S')
-//                 console.log(xofS);
-                
-//                 if (element === "S" && array.length === 12) {
-//                      console.log('S');
-//                     array.splice(xofS, 1, '.')
-//                     array.splice(xofS+1, 1, "S")
-//                 // console.log(elementParent[0]);
-//             //         elementParent.splice(xofS, 1, '.')
-//                       createMaze()
-//                       xofS += 1
-//             //     //     //console.log(elementParent[xofS+2]);
-//             //     //     break
-//             //     // }
-//             //     // else if (elementParent[xofS-1] === "*") {
-//             //     //     break;
-//               } 
-//             }
-//         }
-//         x += 1
-
-//     }
-// })
+window.addEventListener('keydown', (event) => {
+    if( event.key === " ") {
+    } 
+})
